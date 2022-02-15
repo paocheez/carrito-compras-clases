209590d509f56760c476659de3e938546da17c16
Paola Conde <pao.cheez@gmail.com>
2022-02-15 17:46:37 -0600

    creation of product, products, button and layout components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react';
+import Productos from './components/Productos';
+import Layout from './components/Layout';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    productos: [
+      { name: 'Tomate', price: 1500, img: '/assets/tomate.jpg'},
+      { name: 'Arbejas', price: 2500, img: '/assets/arbejas.jpg'},
+      { name: 'Lechuga', price: 500, img: '/assets/lechuga.jpg'}
+    ]
+  }
+
+  render(){
+    return(
+      <div>
+        <Layout>
+          <Productos
+          agregarAlCarro={()=>console.log('No hace nada')}
+          productos={this.state.productos}
+        />
+        </Layout>
+      </div>
+    )
+  }
 }
 
 export default App;
